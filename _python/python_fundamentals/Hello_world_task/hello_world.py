b8288f55258c2332c1a0bf70b90a657050009684
Mahmoud <mod2saman@gmail.com>
2026-04-10 23:37:46 +0300

    hello world task

---
+++ _python/python_fundamentals/Hello_world_task/hello_world.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+name="noella"
+print("hello",name)
+print("hello"+name)
+
+name=42
+print("hello",name)
+print("hello "+str(name))
+
+fav_food1="pizza"
+fav_food2="sushi"
+print(f"I love to eat {fav_food1} and {fav_food2}")
+print("I love to eat {} and {}".format(fav_food1,fav_food2))
